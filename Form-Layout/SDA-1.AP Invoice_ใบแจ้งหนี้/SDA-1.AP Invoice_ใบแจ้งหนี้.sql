a3b630cede5c5012c8a2ddfe09c6b58f72d70872
sattrawutd <sattrawut@sala-daeng.com>
2026-04-09 11:52:45 +0700

    idfhtgioh

--- Form-Layout/SDA-1.AP Invoice_ใบแจ้งหนี้/SDA-1.AP Invoice_ใบแจ้งหนี้.sql
+++ Form-Layout/SDA-1.AP Invoice_ใบแจ้งหนี้/SDA-1.AP Invoice_ใบแจ้งหนี้.sql
@@ -1,4 +1,4 @@
-SELECT OINV.DocEntry, OINV.DocDate, OINV.DocDueDate, OINV.DocStatus, OINV.DocType, OINV.DocNum ,OINV.CardCode, OINV.CardName ,OINV.DocTotal 
+SELECT OINV.DocEntry, OINV.DocDate, OINV.DocDueDate, OINV.DocStatus, OINV.DocType, OINV.DocNum ,OINV.CardCode, OINV.CardName ,OINV.DocTotal,OINV.DocDate
 
 From OINV
 Where OINV.DocDate >= '2026-01-01'
